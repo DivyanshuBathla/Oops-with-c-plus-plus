eb454c8a1314491b82b15a6305cafac0f3046010
Divyanshu Bathla <91410787+DivyanshuBathla@users.noreply.github.com>
2021-10-20 23:36:13 +0530

    Add files via upload

---
+++ practical_07/task6.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int x;
+   cout<<"Enter the size of square matrix=";
+   cin>>x;
+   cout<<endl;
+   char arr[x][x];
+   for(int i=0;i<x;i++)
+   {
+   	for(int j=0;j<x;j++)
+  	{
+           if(i<=j)
+           arr[i][j]='*';
+           else
+           arr[i][j]=' ';
+   	}
+   }
+   for(int i=0;i<x;i++)
+   {
+   	for(int j=0;j<x;j++)
+  	{
+          cout<<arr[i][j]<<" ";
+   	}
+   	cout<<"\n";
+   }
+ cout<<"\n";
+}
---
+++ practical_07/task8.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int x;
+   cout<<"Enter the size of square matrix=";
+   cin>>x;
+   cout<<endl;
+   char arr[x][x];
+   int m=0,l=0;
+   for(int i=x-1;i>=0;i--)
+   {
+   	for(int k=0;k<=i;k++)
+  	{
+           arr[l][m++]='*';
+   	}
+   	for(int j=i;j<x-1;j++)
+   	{
+   	    arr[l][m++]=' ';
+   	}
+   	l++;
+   	m=0;
+   }
+   for(int i=0;i<x;i++)
+   {
+   	for(int j=0;j<x;j++)
+  	{
+          cout<<arr[i][j]<<" ";
+   	}
+   	cout<<"\n";
+   }
+ cout<<"\n";
+}
