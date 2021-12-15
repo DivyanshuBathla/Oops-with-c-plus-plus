00ce2364690ac0c62afc8964c13c0dc1dbe37280
Divyanshu Bathla <91410787+DivyanshuBathla@users.noreply.github.com>
2021-12-15 10:00:46 +0530

    Create task2_c.cpp

---
+++ opps with c++/practical12/task2_c.cpp
@@ -0,0 +1,36 @@
+    
+    #include<iostream>
+class A
+{
+    protected:
+    int a;
+    public:
+    A(int a)
+    { 
+      this->a=a;
+     }
+     
+};
+
+class B:public A
+{
+    int b;
+    public:
+    
+    B(int b):A(b)
+    { 
+       this->b=b;
+     }
+     void display()
+     {
+         std::cout<<"a="<<a;
+         std::cout<<"\nb="<<b;
+     }
+};
+
+
+int main()
+{
+    B obj(15); 
+    obj.display();
+}
