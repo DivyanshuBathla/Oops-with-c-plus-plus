57543d4536c61ad4855869033ac8225186ae65bd
Divyanshu Bathla <91410787+DivyanshuBathla@users.noreply.github.com>
2021-12-28 14:37:01 +0530

    Create task3.cpp

---
+++ opps with c++/Practical15/task3.cpp
@@ -0,0 +1,16 @@
+
+#include <fstream>
+#include<iostream>
+#include<sstream>
+#include<iomanip>
+using namespace std;
+
+int main()
+{
+    double f=3.1547895;
+    cout<<"Using endl:"<<"Abc  "<<endl;
+    cout<<"Using ends:"<<"Abc  "<<ends;                       //only clear buffedidmt go to next line
+    cout<<"Using flush:"<<"Abc  "<<flush<<endl;
+    cout<<"Using setprecision(5)="<<setprecision(5)<<f<<endl;
+    cout<<"Using setw(8)="<<setw(8)<<12345678945<<endl;
+}
